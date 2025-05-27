979a736b96a0d29fcb19b8c0aa15f5ea472600f3
Alexander Oster <alexanderoster@users.noreply.github.com>
2025-05-27 16:15:55 +0200

    Scanlab OIE: Added Selectable Recording Frequency

--- Drivers/ScanLabOIE/Implementation/libmcdriver_scanlaboie_oiedevice.hpp
+++ Drivers/ScanLabOIE/Implementation/libmcdriver_scanlaboie_oiedevice.hpp
@@ -101,8 +101,12 @@ protected:
 	uint32_t m_nDeviceID;
 
 	bool m_bIsConnected;
+	uint64_t m_nPacketReceiveCounter;
+	uint64_t m_nPacketReceiveSkipCounter;
 
 	std::vector<POIEDeviceApp> m_AppList;
+	LibMCDriver_ScanLabOIE::eOIEDeviceDriverType m_DeviceDriverType;
+	LibMCDriver_ScanLabOIE::eOIERecordingFrequency m_RecordingFrequency;
 
 	// RTC 6 Correction File Data
 	bool m_bHasCorrectionData;
@@ -171,6 +175,10 @@ public:
 	bool AppIsRunning();
 
 	void GetRunningApp(std::string& sName, LibMCDriver_ScanLabOIE_uint32& nMajor, LibMCDriver_ScanLabOIE_uint32& nMinor, LibMCDriver_ScanLabOIE_uint32& nPatch);
+
+	void SetRecordingFrequency(const LibMCDriver_ScanLabOIE::eOIERecordingFrequency eFrequency);
+
+	LibMCDriver_ScanLabOIE::eOIERecordingFrequency GetRecordingFrequency();
 
 	void InstallApp(const LibMCDriver_ScanLabOIE_uint64 nAppPackageBufferSize, const LibMCDriver_ScanLabOIE_uint8* pAppPackageBuffer);
 
@@ -256,6 +264,10 @@ public:
 	void StopApp() override;
 
 	bool AppIsRunning() override;
+
+	void SetRecordingFrequency(const LibMCDriver_ScanLabOIE::eOIERecordingFrequency eFrequency) override;
+
+	LibMCDriver_ScanLabOIE::eOIERecordingFrequency GetRecordingFrequency() override;
 
 	void GetRunningApp(std::string & sName, LibMCDriver_ScanLabOIE_uint32 & nMajor, LibMCDriver_ScanLabOIE_uint32 & nMinor, LibMCDriver_ScanLabOIE_uint32 & nPatch) override;
 
